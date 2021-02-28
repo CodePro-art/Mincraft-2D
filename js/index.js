9abed5611dbb249aff9af50b35762e8610e0cf65
Netanel Mazuz <76904298+CodePro-art@users.noreply.github.com>
2021-02-28 04:07:12 +0200

    first commit
    2D Minecraft weekend project

---
+++ js/index.js
@@ -0,0 +1,249 @@
+const rows = 20;
+const cols = 25;
+let size = rows*cols;
+let grass = rows-6;
+let ground = rows-5;
+let lava = rows-1;
+let screen = document.querySelector('.game-screen');
+const map = document.querySelector('#wrapper-2');
+const tool = document.querySelectorAll('.tool');
+const inventory = document.querySelectorAll('.inv');
+random = (min,max) => Math.random() * (max - min) + min;
+
+class Minecraft {
+  constructor(width,trees,bushes) {
+    this.width = width;
+    this.trees = trees;
+    this.bushes = bushes;
+    this.selectedTool ="";
+    this.selectedItem ="";
+    this.inventoryCounter ={
+      dirt: 0,
+      brick: 0,
+      coal: 0,
+      ice: 0,
+      gold: 0,
+      grass: 0,
+      silver: 0,
+      glass: 0,
+      sand: 0,
+      snow: 0,
+      stone: 0,
+      wood: 0
+    };
+  }
+  // function to initialize world
+  initialize(){
+    console.log("init");
+    for(let i=0; i<rows ;i++){
+      for(let j=0; j<cols ;j++){
+        let box = document.createElement('button');
+        box.classList.add('box');
+        if(i<grass-1)
+          box.classList.add('sky');
+        if(i<grass && i>grass-4 && (j === 15))
+          box.classList.add('wood');
+        if((i<grass-3 && i>grass-7) && (j>13 && j<17))
+          box.classList.add('leaves');
+        if((i == grass-1 && j === 8) || (i == grass-1 && j === 9))
+            box.classList.add('bush');
+        if((i == grass-1 && (j === 20 || j===21)))
+            box.classList.add('stone');
+        if(i === grass )
+          box.classList.add('grass');
+        if(i>=ground && i<lava){
+          (j%17) || (i !== ground+3) ? box.classList.add('ground') : box.classList.add('gold');
+          (j%12) || (i !== ground+1) ? box.classList.add('ground') : box.classList.add('silver');
+        }
+        if(i === rows-1)
+          box.classList.add('lava'); 
+        box.setAttribute('row',`${j}`);
+        box.setAttribute('col',`${i}`);
+
+        screen.appendChild(box);
+        setTimeout(()=> box.classList.add("animate"), (i+j)*100);
+      }
+    }
+  }
+ 
+  generateRandom(bush,tree){
+    const maxBushes = Math.floor(cols/3);
+    const maxTrees = Math.floor(cols/5);
+    bush = bush > maxBushes ? maxBushes : bush;
+    tree = tree > maxTrees ? maxTrees : tree;
+    let rand = random(0,cols-1);
+  }
+
+  resetMap(){
+    console.log("remove!");
+    screen.remove();
+    screen = document.createElement('section');
+    screen.classList.add('game-screen');
+    screen.setAttribute("id","screen");
+    map.appendChild(screen);
+    this.initialize();
+  }
+}
+
+// initialize game
+let game = new Minecraft(1);
+game.initialize();
+
+// clense cursor
+document.body.addEventListener("keydown", function(e){
+  var keycode = e.charCode || e.keyCode;
+  switch ( keycode ) {
+    default: 
+      break;
+    case 27:
+      clearCursor();
+      break; 
+  }
+})
+
+// function to clear cursor to default
+function clearCursor(){
+  tiles.forEach((box)=>{
+    box.classList.remove('cursor-shovel');
+    box.classList.remove('cursor-pickaxe');
+    box.classList.remove('cursor-axe');
+    box.classList.remove('cursor-hoe');
+  });
+}
+// reset
+function reset(){
+  document.getElementById('rst-btn').classList.add('rst');
+  game.resetMap();
+  setTimeout(() => {
+  document.getElementById('rst-btn').classList.remove('rst');
+  },500)
+}
+
+// cursor
+let tiles = document.querySelectorAll('.box');
+tool.forEach((e)=>{
+  e.addEventListener('click',()=>{
+    if(e.classList.contains('axe')){
+      game.selectedTool ="axe";
+      tiles.forEach((box)=>{
+        box.classList.add('cursor-axe');
+        box.classList.remove('cursor-pickaxe');
+        box.classList.remove('cursor-shovel');
+        box.classList.remove('cursor-hoe');
+      });
+  // url(../img/axe.png), auto;
+    }
+    if(e.classList.contains('pickaxe')){
+      game.selectedTool ="pickaxe";
+      tiles.forEach((box)=>{
+        box.classList.add('cursor-pickaxe');
+        box.classList.remove('cursor-axe');
+        box.classList.remove('cursor-shovel');
+        box.classList.remove('cursor-hoe');
+      });
+    }
+    if(e.classList.contains('shovel')){
+      game.selectedTool ="shovel";
+      tiles.forEach((box)=>{
+        box.classList.add('cursor-shovel');
+        box.classList.remove('cursor-pickaxe');
+        box.classList.remove('cursor-axe');
+        box.classList.remove('cursor-hoe');
+      });
+    }
+    if(e.classList.contains('hoe')){
+      game.selectedTool ="hoe";
+      tiles.forEach((box)=>{
+        box.classList.add('cursor-hoe');
+        box.classList.remove('cursor-pickaxe');
+        box.classList.remove('cursor-shovel');
+        box.classList.remove('cursor-axe');
+      });
+    }
+  })
+});
+
+// tiles event
+tiles.forEach((box)=>{
+  box.addEventListener('click',()=>{
+    if(game.selectedTool === 'shovel'){
+      if(box.classList.contains('ground')){
+        box.classList.remove('ground');
+        game.inventoryCounter.dirt++;
+        console.log(game.inventoryCounter.dirt);
+      }
+      if(box.classList.contains('grass')){
+        box.classList.remove('grass');
+        game.inventoryCounter.grass++;
+      }    
+    }
+    if(game.selectedTool === 'axe'){
+      if(box.classList.contains('leaves')){
+        box.classList.remove('leaves');
+        game.inventoryCounter.grass++;
+      }
+      if(box.classList.contains('wood')){
+        box.classList.remove('wood');
+        game.inventoryCounter.wood++;
+      }
+    }
+    if(game.selectedTool === 'pickaxe'){
+      if(box.classList.contains('gold')){
+        box.classList.remove('gold');
+        game.inventoryCounter.gold++;
+      }
+      if(box.classList.contains('stone')){
+        box.classList.remove('stone');
+        game.inventoryCounter.stone++;
+      }
+      if(box.classList.contains('silver')){
+        box.classList.remove('silver');
+        game.inventoryCounter.silver++;
+      }
+      if(box.classList.contains('diamond')){
+        box.classList.remove('diamond');
+        game.inventoryCounter.diamond++;
+      }
+    }
+    if(game.selectedTool === 'hoe'){
+      if(box.classList.contains('lava')){
+        box.classList.remove('lava');
+        game.inventoryCounter.coal++;
+      }
+      if(box.classList.contains('stone')){
+        box.classList.remove('stone');
+        game.inventoryCounter.brick++;
+      }
+      if(box.classList.contains('water')){
+        box.classList.remove('water');
+        game.inventoryCounter.ice++;
+      }
+      if(box.classList.contains('ground')){
+        box.classList.remove('ground');
+        game.inventoryCounter.sand++;
+      }
+      if(box.classList.contains('sand')){
+        box.classList.remove('sand');
+        game.inventoryCounter.glass++;
+      }
+    }
+
+    updateInventory();
+  });
+});
+
+// function to update inventory
+function updateInventory(){
+  inventory.forEach((item)=>{
+    if (item.classList[1].slice(4))
+    item.innerHTML = game.inventoryCounter[item.classList[1].slice(4)];
+  });
+}
+
+
+
+
+
+
+
+
---
+++ js/menu.js
@@ -0,0 +1,136 @@
+//initialize vars  
+let arrowRight = document.querySelector('.arrow-right'),
+    arrowLeft = document.querySelector('.arrow-left'),
+    enterKey = document.querySelector('.active'),
+    menuAngle = 0,
+    iconAngle = 0,
+    icons = document.querySelectorAll('#hex'),
+    hexWrapper = document.querySelector('.hex-row-wrapper'),
+    volume = document.querySelector('.volume'),
+    debounce,
+    delay = 50;
+
+//movenment controls
+function moveRight() {
+  clearTimeout(debounce);
+  debounce = setTimeout(function(){
+    menuAngle += 60;
+    iconAngle -= 60;
+    //rotate icon
+    for(let i = 0; i < icons.length; i ++){
+      icons[i].setAttribute("style", 'transform: rotate(' + iconAngle + 'deg);');
+    }
+    //rotate menu
+    hexWrapper.setAttribute("style", 'transform: rotate(' + menuAngle + 'deg);' );
+    // Select next item
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    selected.classList.contains('one') ? selected.parentElement.lastElementChild.classList.add('selected') : selected.previousElementSibling.classList.add('selected');
+  },delay)
+};
+
+function moveLeft() {
+  clearTimeout(debounce);
+  debounce = setTimeout(function(){
+    menuAngle -= 60;
+    iconAngle += 60;
+      //rotate icon
+      for(let i = 0; i < icons.length; i ++){
+        icons[i].setAttribute("style", 'transform: rotate(' + iconAngle + 'deg);');
+      }
+      //rotate menu
+      hexWrapper.setAttribute("style", 'transform: rotate(' + menuAngle + 'deg);' );
+      // Select the current
+      let selected = document.querySelector('.selected');
+      selected.classList.remove('selected');
+      selected.classList.contains('six') ? selected.parentElement.firstElementChild.classList.add('selected') : selected.nextElementSibling.classList.add('selected');
+  }, delay);
+};
+  
+// Enter selected
+function enterSelected() {
+  let selected = document.querySelector('.selected');
+  selected.click();
+};
+
+//click listeners
+arrowLeft.addEventListener("click", function(){
+  moveLeft();
+});
+arrowRight.addEventListener("click", function(){
+  moveRight();
+});
+enterKey.addEventListener("click", function(){
+  enterSelected();
+});
+
+//key listenters
+document.body.addEventListener("keydown", function(e){
+  var keycode = e.charCode || e.keyCode;
+  switch ( keycode ) {
+    default: 
+      break;
+    case 13:
+      enterSelected();
+      break; 
+    case 37:
+      moveLeft();
+      break; 
+    case 39:  
+      moveRight();
+      break; 
+  }
+});
+  
+// music play
+let music = document.getElementById('player');
+let button =  document.querySelector('.six');
+let playStop =  document.querySelector('.start');
+let musicIcon =  document.querySelector('.fa-music');
+let checkbox = document.querySelector('.checkbox');
+music.loop = true;
+music.load();
+
+button.addEventListener('click',()=>{
+  checkbox.checked = checkbox.checked ? false: true;
+  checkbox.checked ? musicIcon.classList.add('started') : musicIcon.classList.remove('started');
+  music.paused ? music.play() : music.pause();
+})
+
+playStop.addEventListener('click',()=>{
+  console.log(playStop);
+  if(checkbox.checked){
+    playStop.firstElementChild.style.display = "none";
+    playStop.lastElementChild.style.display = "block";
+    musicIcon.classList.add('started');
+    checkbox.checked = false;
+  }else{
+    checkbox.checked = true;
+    playStop.lastElementChild.style.display = "none";
+    playStop.firstElementChild.style.display = "block";
+    musicIcon.classList.remove('started');
+  }
+  music.paused ? music.play() : music.pause();
+})
+
+// Play link
+let play = document.querySelector('.one');
+let back = document.querySelector('.back');
+let landingPage = document.querySelector('#wrapper-1');
+let gameConsole = document.querySelector('#wrapper-2');
+
+
+play.addEventListener('click',()=>{
+  gameConsole.style.display = "flex";
+  setTimeout(() => {
+  landingPage.style.display = "none";
+  },500)
+})
+
+
+back.addEventListener('click',()=>{
+  landingPage.style.display = "flex";
+  gameConsole.style.display = "none";
+})
+
+
